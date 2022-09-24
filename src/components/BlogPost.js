0b0819bcb4fa86afb4405bbc7a339a339f527fe7
wanjiku <annritawanjiku77@gmail.com>
2022-09-24 06:40:46 +0300

    completed assignment

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -3,6 +3,7 @@ import BlogContent from "./BlogContent";
 import Comment from "./Comment";
 
 function BlogPost() {
+  
   return (
     <div id="blog-post">
       <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
@@ -12,5 +13,6 @@ function BlogPost() {
     </div>
   );
 }
+
 
 export default BlogPost;
